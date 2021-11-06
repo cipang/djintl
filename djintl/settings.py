4ab80ef8b3969e4b60d5a850fba5fc977ecc9b54
Patrick Pang <mail@patrickpang.net>
2021-11-06 08:47:13 +0800

    Minor fixes

--- djintl/settings.py
+++ djintl/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1s)y8m$xk3hk&%x#8(*9q&9yy!p00!3=mn0*&m-cvd=aq1f$$d'
+SECRET_KEY = 'django-insecure-1s)aaaaaaaaa&%x#8(*9q&9yy!p00!3=mn0*&m-cvd=aq1f$$d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
